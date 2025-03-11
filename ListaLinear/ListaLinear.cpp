1f33eec2375c8fa68408f98d7c24f5bf5a1c1db6
Brenno <brennogrodrigues@yahoo.com.br>
2025-03-11 10:58:31 -0300

    Atv 3

--- ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear.cpp
@@ -119,11 +119,24 @@ void inserirElemento()
 	}
 
 }
-
 void excluirElemento()
 {
+	int valor;
+	cout << "Digite o elemento que deseja excluir: ";
+	cin >> valor;
 
+	int pos = posicaoElemento(valor);
 
+	if (pos != -1) {
+		for (int i = pos; i < nElementos - 1; i++) {
+			lista[i] = lista[i + 1];
+		}
+		nElementos--;
+		cout << "Elemento " << valor << " excluído com sucesso!" << endl;
+	}
+	else {
+		cout << "Elemento não encontrado." << endl;
+	}
 }
 
 void buscarElemento()
